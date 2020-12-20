85530e09743ed6fcc025ac9fdd91bb51895de401
Chonwai Un, Edison <chonwaiun@gmail.com>
2020-12-20 23:41:23 +0800

    Add: Added the edgeDetection function.

--- rec.py
+++ rec.py
@@ -7,16 +7,42 @@ ap.add_argument("-i", "--image", required=True, help="path to input image")
 args = vars(ap.parse_args())
 
 
-def cv_show(name, img):
+def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+    if width is None and height is None:
+        return image
+    if width is None:
+        r = height / float(h)
+        dim = (int(w*r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h*r))
+    resized = cv2.resize(image, dim, interpolation=inter)
+    return resized
+
+
+def show(name, img):
     cv2.imshow(name, img)
-    cv2.waitKey(1000)
+    cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+
+def edgeDetection(img):
+    ratio = img.shape[0] / 500.0
+    orig = img.copy()
+    image = resize(orig, width=500)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(blur, 75, 200)
+    show('image', edged)
 
 
 def init():
     print("HIHI")
     image = cv2.imread(args["image"])
-    cv_show('image', image)
+    # show('image', image)
+    edgeDetection(image)
 
 
 if __name__ == "__main__":
