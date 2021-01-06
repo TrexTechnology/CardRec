b5107d38b3368ccd37bfcbda074fb0958ffbbc59
Chonwai <chonwaiun@gmail.com>
2021-01-06 18:19:55 +0800

    Add: Added the config for the Darknet.

--- rec.py
+++ rec.py
@@ -1,3 +1,5 @@
+import os
+import json
 import numpy as np
 import argparse
 import cv2
@@ -6,9 +8,20 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="path to input image")
 args = vars(ap.parse_args())
 
+def readResultCoordinates(imagePath):
+    f = open('result/result.json')
+    data = json.load(f)
+    f.close()
+    print(data)
 
 def init():
-    image = cv2.imread(args["image"])
+    imagePath = args["image"]
+    stream = os.popen("./darknet/darknet detector test config/obj.data config/yolov4-tiny-15.cfg config/yolov4-tiny-15_50000.weights -dont_show -ext_output " + imagePath + " -out result/result.json")
+    if (stream.read()):
+        print('Good!')
+        readResultCoordinates(imagePath)
+    else:
+        print('Fail')
 
 if __name__ == "__main__":
     init()
