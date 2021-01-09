22aa2e681197f81cee8ae037bab2f0c1ba7c448c
Chonwai <chonwaiun@gmail.com>
2021-01-09 11:18:34 +0800

    Add: Added some functions on Utils.

--- rec.py
+++ rec.py
@@ -3,31 +3,35 @@ import json
 import numpy as np
 import argparse
 import utils.utils as Utils
-# import pytesseract
+import pytesseract
 from PIL import Image
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="path to input image")
 args = vars(ap.parse_args())
 
-def readResultCoordinates(imagePath):
-    f = open('result/result.json')
+def getResultList(resultPath, imagePath):
+    f = open(resultPath)
     data = json.load(f)
-    print(data)
+    f.close()
     img = Image.open(imagePath)
     origin_width, origin_height = img.size
-    object1 = data[0]
-    x1,x2,y1,y2 = Utils.utils.convergeRelativeCoordinateToAbsoultCoordinates(origin_width, origin_height, object1['objects'][0]['relative_coordinates']['center_x'], object1['objects'][0]['relative_coordinates']['center_y'], object1['objects'][0]['relative_coordinates']['width'], object1['objects'][0]['relative_coordinates']['height'])
-    img2 = img.crop((x1,y1,x2,y2))
-    img2.save("./img2.jpg")
-    f.close()
+    result = Utils.Utils.getAllClassesAndCoordinates(origin_width, origin_height, data)
+    return result
 
 def init():
     imagePath = args["image"]
+    img = Image.open(imagePath)
     stream = os.popen("./darknet/darknet detector test config/obj.data config/yolov4-tiny-15.cfg config/yolov4-tiny-15_50000.weights -dont_show -ext_output " + imagePath + " -out result/result.json")
     if (stream.read()):
         print('Good!')
-        readResultCoordinates(imagePath)
+        result_list = getResultList('result/result.json', imagePath)
+        i = 0
+        for item in result_list:
+            cropped_image = Utils.Utils.cropImage(img, item['relative_coordinates'][0], item['relative_coordinates'][1], item['relative_coordinates'][2], item['relative_coordinates'][3])
+            cropped_image.save("./img" + str(i) + ".jpg")
+            i = i + 1
+            pass
     else:
         print('Fail')
 
--- utils/utils.py
+++ utils/utils.py
@@ -1,9 +1,33 @@
-class utils:
+from PIL import Image
+
+
+class Utils:
     @staticmethod
     def convergeRelativeCoordinateToAbsoultCoordinates(origin_width, origin_height, center_x, center_y, width, height):
         x1 = (origin_width * center_x) - (origin_width * width) / 2
         x2 = (origin_width * center_x) + (origin_width * width) / 2
         y1 = (origin_height * center_y) - (origin_width * height) / 2
         y2 = (origin_height * center_y) + (origin_width * height) / 2
-        return x1, x2, y1, y2
+        return x1, y1, x2, y2
     pass
+
+    @staticmethod
+    def getAllClassesAndCoordinates(origin_width, origin_height, result):
+        object_list = []
+        objects = result[0]['objects']
+        for item in objects:
+            object_list_item = {}
+            object_list_item['class_id'] = item['class_id']
+            object_list_item['class'] = item['name']
+            object_list_item['relative_coordinates'] = Utils.convergeRelativeCoordinateToAbsoultCoordinates(
+                origin_width, origin_height, item['relative_coordinates']['center_x'], item['relative_coordinates']['center_y'], item['relative_coordinates']['width'], item['relative_coordinates']['height'])
+            object_list_item['result_text'] = ''
+            object_list.append(object_list_item)
+            pass
+        return object_list
+    pass
+
+    @staticmethod
+    def cropImage(image, x1, y1, x2, y2):
+        cropped_image = image.crop((x1, y1, x2, y2))
+        return cropped_image
