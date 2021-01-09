77f7373083be80ad7f34585ad10d9d7a775b7c0d
Chonwai Un, Edison <chonwaiun@gmail.com>
2021-01-09 16:53:36 +0800

    Update: Updated the rec.py.

--- rec.py
+++ rec.py
@@ -1,4 +1,5 @@
 import os
+import platform
 import json
 import numpy as np
 import argparse
@@ -10,30 +11,41 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="path to input image")
 args = vars(ap.parse_args())
 
+
 def getResultList(resultPath, imagePath):
     f = open(resultPath)
     data = json.load(f)
     f.close()
     img = Image.open(imagePath)
     origin_width, origin_height = img.size
-    result = Utils.Utils.getAllClassesAndCoordinates(origin_width, origin_height, data)
+    result = Utils.Utils.getAllClassesAndCoordinates(
+        origin_width, origin_height, data)
     return result
+
 
 def init():
     imagePath = args["image"]
     img = Image.open(imagePath)
-    stream = os.popen("./darknet/darknet detector test config/obj.data config/yolov4-tiny-15.cfg config/yolov4-tiny-15_50000.weights -dont_show -ext_output " + imagePath + " -out result/result.json")
+    if (platform.system() == 'Windows'):
+        stream = os.popen(".\darknet\darknet detector test config/obj.data config/yolov4-tiny-15.cfg config/yolov4-tiny-15_50000.weights -dont_show -ext_output " +
+                          imagePath + " -out result/result.json")
+    else:
+        stream = os.popen("./darknet/darknet detector test config/obj.data config/yolov4-tiny-15.cfg config/yolov4-tiny-15_50000.weights -dont_show -ext_output " +
+                          imagePath + " -out result/result.json")
     if (stream.read()):
         print('Good!')
         result_list = getResultList('result/result.json', imagePath)
         i = 0
         for item in result_list:
-            cropped_image = Utils.Utils.cropImage(img, item['relative_coordinates'][0], item['relative_coordinates'][1], item['relative_coordinates'][2], item['relative_coordinates'][3])
+            cropped_image = Utils.Utils.cropImage(
+                img, item['relative_coordinates'][0], item['relative_coordinates'][1], item['relative_coordinates'][2], item['relative_coordinates'][3])
             cropped_image.save("./img" + str(i) + ".jpg")
             i = i + 1
             pass
     else:
         print('Fail')
 
+
 if __name__ == "__main__":
     init()
+    # print("Hi")
